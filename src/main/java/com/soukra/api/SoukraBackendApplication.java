2cc886a4547162af5f38032ec3602f1045bc7e59
Lockycredo <beguedoucredo312@gmail.com>
2026-09-23 17:41:19 +0000

    feat: Restructuration du projet Spring Boot à la racine

---
+++ src/main/java/com/soukra/api/SoukraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soukra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoukraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoukraBackendApplication.class, args);
+	}
+
+}
